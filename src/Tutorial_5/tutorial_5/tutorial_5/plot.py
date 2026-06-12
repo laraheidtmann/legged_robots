e5e0991a86dbc18539d08672ff0344374ad2e96c
laraheidtmann <ge78puq@mytum.de>
2026-06-12 15:11:45 +0000

    finished Tutorial 5 exercise 2

---
+++ src/Tutorial_5/tutorial_5/tutorial_5/plot.py
@@ -0,0 +1,65 @@
+import sqlite3
+import numpy as np
+import matplotlib.pyplot as plt
+from rosidl_runtime_py.utilities import get_message
+from rclpy.serialization import deserialize_message
+import rosbag2_py
+
+def read_pose_topic(bag_path, topic_name):
+    storage_options = rosbag2_py.StorageOptions(uri=bag_path, storage_id="mcap")
+    converter_options = rosbag2_py.ConverterOptions("", "")
+    reader = rosbag2_py.SequentialReader()
+    reader.open(storage_options, converter_options)
+
+    topic_types = reader.get_all_topics_and_types()
+    type_map = {t.name: t.type for t in topic_types}
+
+    times, xs, ys = [], [], []
+
+    while reader.has_next():
+        topic, data, t = reader.read_next()
+        if topic == topic_name:
+            msg_type = get_message(type_map[topic])
+            msg = deserialize_message(data, msg_type)
+            times.append(t * 1e-9)  # ns -> s
+            xs.append(msg.point.x)
+            ys.append(msg.point.y)
+
+    return np.array(times), np.array(xs), np.array(ys)
+
+
+#bag_path = "/home/ubuntu/legged_robots/rosbag2_2026_06_12-14_47_00"  # the folder, not the .db3 file
+#bag_path= "rosbag2_2026_06_12-14_47_00"
+#bag_path="rosbag2_2026_06_12-15_03_36"
+bag_path="rosbags/rosbag2_2026_06_12-15_09_10"
+t_zmp, x_zmp, y_zmp = read_pose_topic(bag_path, "/zmp")
+t_cmp, x_cmp, y_cmp = read_pose_topic(bag_path, "/cmp")
+t_cp,  x_cp,  y_cp  = read_pose_topic(bag_path, "/cp")
+t_com, x_com, y_com = read_pose_topic(bag_path, "/com")
+
+# normalize time to start at 0
+t0 = t_zmp[0]
+t_zmp -= t0; t_cmp -= t0; t_cp -= t0; t_com -= t0
+
+fig, axes = plt.subplots(2, 1, figsize=(10, 8), sharex=True)
+
+axes[0].plot(t_com, x_com, label="CoM x")
+axes[0].plot(t_zmp, x_zmp, label="ZMP x")
+axes[0].plot(t_cmp, x_cmp, label="CMP x")
+axes[0].plot(t_cp,  x_cp,  label="CP/DCM x")
+axes[0].set_ylabel("x [m]")
+axes[0].legend()
+axes[0].grid(True)
+
+axes[1].plot(t_com, y_com, label="CoM y")
+axes[1].plot(t_zmp, y_zmp, label="ZMP y")
+axes[1].plot(t_cmp, y_cmp, label="CMP y")
+axes[1].plot(t_cp,  y_cp,  label="CP/DCM y")
+axes[1].set_xlabel("time [s]")
+axes[1].set_ylabel("y [m]")
+axes[1].legend()
+axes[1].grid(True)
+
+plt.tight_layout()
+plt.savefig("ground_reference_points.png")
+plt.show()
--- src/Tutorial_5/tutorial_5/tutorial_5/t51.py
+++ src/Tutorial_5/tutorial_5/tutorial_5/t51.py
@@ -21,7 +21,7 @@ import rclpy
 from rclpy.node import Node
 import tf2_ros
 from sensor_msgs.msg import JointState
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, PointStamped
 from tf2_ros import TransformBroadcaster
 from geometry_msgs.msg import TransformStamped
 
@@ -51,14 +51,50 @@ class Talos(Robot):
         self.State="right"
         self.push_state_start_time=None
 
-        # TODO call base class constructor
-        # TODO add publisher
-        # TODO add tf broadcaster
-        pass
+        self.zmp_publisher = self.node.create_publisher(PointStamped, "zmp", 10)
+        self.cmp_publisher = self.node.create_publisher(PointStamped, "cmp", 10)
+        self.cp_publisher = self.node.create_publisher(PointStamped, "cp", 10)
+        self.com_publisher = self.node.create_publisher(PointStamped, "com", 10)
+
+      
 
     def update(self):
         super().update()
         
+    def publish_ground_reference_points(self, zmp, cmp, cp,com):
+        stamp = self.node.get_clock().now().to_msg()
+
+        zmp_msg = PointStamped()
+        zmp_msg.header.stamp = stamp
+        zmp_msg.header.frame_id = "world"
+        zmp_msg.point.x = float(zmp[0])
+        zmp_msg.point.y = float(zmp[1])
+        zmp_msg.point.z = float(zmp[2])
+        self.zmp_publisher.publish(zmp_msg)
+
+        cmp_msg = PointStamped()
+        cmp_msg.header.stamp = stamp
+        cmp_msg.header.frame_id = "world"
+        cmp_msg.point.x = float(cmp[0])
+        cmp_msg.point.y = float(cmp[1])
+        cmp_msg.point.z = float(cmp[2])
+        self.cmp_publisher.publish(cmp_msg)
+
+        cp_msg = PointStamped()
+        cp_msg.header.stamp = stamp
+        cp_msg.header.frame_id = "world"
+        cp_msg.point.x = float(cp[0])
+        cp_msg.point.y = float(cp[1])
+        cp_msg.point.z = float(cp[2])
+        self.cp_publisher.publish(cp_msg)
+
+        com_msg = PointStamped()
+        com_msg.header.stamp = stamp
+        com_msg.header.frame_id = "world"
+        com_msg.point.x = float(com[0])
+        com_msg.point.y = float(com[1])
+        com_msg.point.z = float(com[2])
+        self.com_publisher.publish(com_msg)
     
     def publish(self,T_frame_w):
         msg = JointState()
@@ -115,7 +151,7 @@ class Talos(Robot):
             force=np.zeros(3)
             
         elif self.State=="finished":
-            return self.State, force#finished
+            return self.State, force #finished
 
 
         elif elapsed < (t_period +t_push):
@@ -144,13 +180,47 @@ class Talos(Robot):
 ################################################################################
 
 
-def estimate_zmp():
-    pass
-def estimate_cmp():
-    pass
-def estimate_cp():
-    pass
+def estimate_zmp(f_ankle, tau_ankle,d):
+    #inputs: 
+    # - f_ankle force measured by the sensor
+    # - tau_ankle torque measured by sensors
+    #returns:  ZMP of foot w.r.t. foot coordinate frame
+    p_x_foot=(-tau_ankle[1]-f_ankle[0]*d)/f_ankle[2]
+    p_y_foot=(-tau_ankle[0]-f_ankle[1]*d)/f_ankle[2]
+    p_z_foot=0.0
+    ZMP=[p_x_foot,p_y_foot, p_z_foot]
 
+    return ZMP
+
+def estimate_joint_zmp(ZMP_left_world,ZMP_right_world,f_l_world,f_r_world):
+   
+    p_x_foot=(ZMP_right_world[0]* f_r_world[2] + ZMP_left_world[0]*f_l_world[2])/ (f_r_world[2]+ f_l_world[2])
+    p_y_foot=(ZMP_right_world[1]* f_r_world[2] + ZMP_left_world[1]*f_l_world[2])/ (f_r_world[2]+ f_l_world[2])
+    p_z_foot=0.0
+    ZMP=[p_x_foot,p_y_foot, p_z_foot]
+
+    return ZMP
+
+
+    
+def estimate_cmp(x,f):
+    #inputs: 
+    # - f ground reaction force
+    # - x position of CoM in world coordinate frame
+    #returns:  r CMP in world coordinate frame
+    r_x=x[0]- (f[0]/f[2])*x[2]
+    r_y=x[1]- (f[1]/f[2])*x[2]
+    r_z=0.0
+    CMP=[r_x,r_y, r_z]
+    
+    return CMP
+    
+def estimate_cp(x_CoM, x_CoM_dot, omega):
+    # Capture point (CP) / DCM
+    xi_x = x_CoM[0] + x_CoM_dot[0] / omega
+    xi_y = x_CoM[1] + x_CoM_dot[1] / omega
+    return [xi_x, xi_y, 0.0]
+    
 def main(): 
     rclpy.init()
     node = rclpy.create_node('tutorial_4_standing_node')
@@ -174,14 +244,10 @@ def main():
     
     t_publish = 0.0
     push_state = "default"
-    force_magnitude=10
+    force_magnitude=15
 
     pb.enableJointForceTorqueSensor(robot.id(), robot.jointNameIndexMap()["leg_right_6_joint"], True)
     pb.enableJointForceTorqueSensor(robot.id(), robot.jointNameIndexMap()["leg_left_6_joint"], True)
-
-   
-
-
 
     while rclpy.ok():
 
@@ -194,8 +260,7 @@ def main():
         rclpy.spin_once(node,timeout_sec=0)
         q=robot.q()
         v=robot.v()
-        t_period=3
-        t_push=1
+
 
         wren = pb.getJointState(robot.id(), robot.jointNameIndexMap()["leg_right_6_joint"])[2]
         wnp = np.array([-wren[0], -wren[1], -wren[2], -wren[3], -wren[4], -wren
@@ -214,6 +279,33 @@ def main():
         H_w_lankle = data.oMf[robot._model.getFrameId("leg_left_6_joint")]
         H_w_rankle = data.oMf[robot._model.getFrameId("leg_right_6_joint")]
 
+        ZMP_left=estimate_zmp(wl_lankle.linear,wl_lankle.angular,0.1)
+        ZMP_left_world=H_w_lankle.rotation @ np.array([ZMP_left[0],ZMP_left[1],ZMP_left[2]-0.1])
+        ZMP_right=estimate_zmp(wr_rankle.linear,wr_rankle.angular,0.1)
+        ZMP_right_world=H_w_rankle.act(np.array([ZMP_right[0],ZMP_right[1],ZMP_right[2]-0.1]))
+
+        f_r_world=H_w_rankle.rotation @ wr_rankle.linear
+        f_l_world=H_w_lankle.rotation @ wl_lankle.linear
+
+        ZMP=estimate_joint_zmp(ZMP_left_world,ZMP_right_world,f_l_world,f_r_world)
+
+        f_ground_reaction=f_r_world+f_l_world
+        CoM_state=tsid_wrapper.comState()
+
+        x_CoM=CoM_state.pos()
+        x_CoM_dot=CoM_state.vel()
+        
+
+        CMP=estimate_cmp(x_CoM,f_ground_reaction)
+
+        CP=estimate_cp(x_CoM,x_CoM_dot,3)
+        robot.publish_ground_reference_points(ZMP,CMP,CP,x_CoM)
+
+
+
+
+        t_period=3
+        t_push=1
         if push_state != "finished":
             push_state,force=robot.pushing_force_state_machine(t,t_period,t_push,force_magnitude)
    
